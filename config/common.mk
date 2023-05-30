a1a28630ba2bdbd9844a83896eee05f52c5d232c
Matt Filetto <matt.filetto@gmail.com>
2023-05-29 22:16:43 -0700

    config: update dexpreopt config for changes from pixel-frameworks
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -201,7 +201,8 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 
 # SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI 
+    SettingsGoogle \
+    SystemUIGoogle
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
